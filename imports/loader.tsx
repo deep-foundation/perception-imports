e3965070bfc540b6f17de63dc7041a6db88e6fdb
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-07-12 17:13:06 +0000

    0.0.4

--- imports/loader.tsx
+++ imports/loader.tsx
@@ -1,8 +1,11 @@
 import { useDeep } from "@deep-foundation/deeplinks/imports/client";
+import { memo } from "react";
 
-export function Loader() {
+export const Loader = memo(function Loader() {
   const deep = useDeep();
   deep.useDeepSubscription({
+    limit: 100,
+    order_by: { id: 'desc' },
   });
   return null;
-}
+}, () => true);
