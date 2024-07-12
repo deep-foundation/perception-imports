344b2064791395b8245a5aa2daadfdbcb19f0ef9
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-07-12 16:12:46 +0000

    0.0.3

--- imports/theme.tsx
+++ imports/theme.tsx
@@ -29,62 +29,6 @@ export const ColorMode = ({
   )
 }
 
-const variantSolid = defineStyle((props) => {
-  const { colorScheme: c } = props
-
-  if (c === "gray") {
-    const bg = mode(`gray.100`, `whiteAlpha.200`)(props)
-
-    return {
-      bg,
-      color: mode(`gray.800`, `whiteAlpha.900`)(props),
-      _hover: {
-        bg: mode(`gray.200`, `whiteAlpha.300`)(props),
-        _disabled: {
-          bg,
-        },
-      },
-      _active: { bg: mode(`gray.300`, `whiteAlpha.400`)(props) },
-    }
-  }
-
-  const accessibleColorMap: { [key: string]: any } = {
-    yellow: {
-      bg: "yellow.400",
-      color: "black",
-      hoverBg: "yellow.500",
-      activeBg: "yellow.600",
-    },
-    cyan: {
-      bg: "cyan.400",
-      color: "black",
-      hoverBg: "cyan.500",
-      activeBg: "cyan.600",
-    },
-  }
-
-  const {
-    bg = `${c}.500`,
-    color = "white",
-    hoverBg = `${c}.600`,
-    activeBg = `${c}.700`,
-  } = accessibleColorMap[c] ?? {}
-
-  const background = mode(bg, `${c}.200`)(props)
-
-  return {
-    bg: background,
-    color: mode(color, `gray.800`)(props),
-    _hover: {
-      bg: mode(hoverBg, `${c}.300`)(props),
-      _disabled: {
-        bg: background,
-      },
-    },
-    _active: { bg: mode(activeBg, `${c}.400`)(props) },
-  }
-})
-
 const config: ThemeConfig = {
   initialColorMode: 'dark',
   useSystemColorMode: true,
