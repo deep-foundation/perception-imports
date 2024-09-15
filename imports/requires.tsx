5d3a0f36c4bcd26ed3a2e8da8e79e369e5518e46
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-09-15 14:55:02 +0000

    0.0.85

--- imports/requires.tsx
+++ imports/requires.tsx
@@ -60,6 +60,14 @@ import { v4 as uuidv4 } from 'uuid';
 import moment from 'moment';
 import * as reactHookForm from 'react-hook-form';
 
+import { TypedIcon } from './icons/typed';
+import { DownIcon } from './icons/down';
+import { UpIcon } from './icons/up';
+import { TypeIcon } from './icons/type';
+import { InIcon } from './icons/in';
+import { OutIcon } from './icons/out';
+import { FromIcon } from './icons/from';
+import { ToIcon } from './icons/to';
 
 import * as ApolloSandbox from '@apollo/sandbox/react/index.cjs';
 import EmojiPicker from 'emoji-picker-react';
@@ -115,7 +123,17 @@ export const requires: any = {
   //   useCytoEditor,
   // },
   '@deep-foundation/deeplinks': deeplinks,
-  '@deep-foundation/perception-imports': perception,
+  '@deep-foundation/perception-imports': {
+    ...perception,
+    TypedIcon,
+    DownIcon,
+    UpIcon,
+    TypeIcon,
+    InIcon,
+    OutIcon,
+    FromIcon,
+    ToIcon,
+  },
   'react-icons/pi': pi,
   'react-icons/bs': bs,
   'react-icons/fi': fi,
