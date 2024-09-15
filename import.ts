d1397140451eda3131652c826f1a4096aa826b8a
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-09-15 13:23:03 +0000

    0.0.76

--- import.ts
+++ import.ts
@@ -1,16 +1,16 @@
-export type { onEnterI, onChangeI, PathI, onDoI, GoContextI, GoI, GoCallbackI, FocusI, ParentsI, AllI, DoI, DoHandleI, onDoObjectI } from './imports/go';
-export { GoContext, ValueContext, GoProvider, noScrollBar, GoCustomContext, useGoCore, useGo, GoCustomProvider } from './imports/go';
-export type { IEditor } from './imports/editor';
-export { Editor, usePsudoResize } from './imports/editor';
+export type { onEnterI, onChangeI, PathI, onDoI, GoContextI, GoI, GoCallbackI, FocusI, ParentsI, AllI, DoI, DoHandleI, onDoObjectI } from './imports/go.js';
+export { GoContext, ValueContext, GoProvider, noScrollBar, GoCustomContext, useGoCore, useGo, GoCustomProvider } from './imports/go.js';
+export type { IEditor } from './imports/editor.js';
+export { Editor, usePsudoResize } from './imports/editor.js';
 
-export type { ClientHandlerRendererProps, ClientHandlerProps, UseClientHandlerProps } from './imports/client-handler';
-export { CatchErrors, evalClientHandler, useHandlersGo, useClientHandler, useFindClientHandler, HandlersGoContext, ClientHandler, HandlerConfigContext, ClientHandlerRenderer } from './imports/client-handler';
+export type { ClientHandlerRendererProps, ClientHandlerProps, UseClientHandlerProps } from './imports/client-handler.js';
+export { CatchErrors, evalClientHandler, useHandlersGo, useClientHandler, useFindClientHandler, HandlersGoContext, ClientHandler, HandlerConfigContext, ClientHandlerRenderer } from './imports/client-handler.js';
 
-export type { ReactHandlerProps } from './imports/react-handler';
-export { WatchLink, ClientHandlerErrorComponent, ClientHandlerUnhandledComponent, ReactHandlerEditor, ReactHandlersProvider, createComponent, ReactHandlerTreeItem, ReactHandlersContext, ReactHandler, } from './imports/react-handler';
+export type { ReactHandlerProps } from './imports/react-handler.js';
+export { WatchLink, ClientHandlerErrorComponent, ClientHandlerUnhandledComponent, ReactHandlerEditor, ReactHandlersProvider, createComponent, ReactHandlerTreeItem, ReactHandlersContext, ReactHandler, } from './imports/react-handler.js';
 
-export { ColorMode, theme } from './imports/theme';
+export { ColorMode, theme } from './imports/theme.js';
 
-export { Packages, usePreload, useHandlersContext, PreloadProviderCore, PreloadProvider, useSymbol, symbol, useLoader, useCanByContain, useChakraColor, getChakraColor, getChakraVar, useChakraVar, PreloadContext, HandlersContext, loader } from './imports/hooks';
+export { Packages, usePreload, useHandlersContext, PreloadProviderCore, PreloadProvider, useSymbol, symbol, useLoader, useCanByContain, useChakraColor, getChakraColor, getChakraVar, useChakraVar, PreloadContext, HandlersContext, loader } from './imports/hooks.js';
 
-export { AutoGuest } from './imports/auto-guest';
+export { AutoGuest } from './imports/auto-guest.js';
