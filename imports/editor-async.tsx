d390d3bb9fe32a174237545f41f36fcc80a5fdc7
Ivan S Glazunov <ivansglazunov@gmail.com>
2024-09-26 03:30:33 +0300

    0.0.127

--- imports/editor-async.tsx
+++ imports/editor-async.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import dynamic from 'next/dynamic';
+import dynamic from 'next/dynamic.js';
  
 export const Editor = dynamic(() => import('./editor').then(m => m.Editor), {
   loading: () => <></>,
